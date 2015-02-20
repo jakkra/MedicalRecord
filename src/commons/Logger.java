04807d7320b5d4b0e1b2a3b0f5f5b42f6c44c9c4
Jakob <mail@jakobkrantz.se>
2015-02-20 16:31:33 +0100

    Logger partly implemented

--- src/commons/Logger.java
+++
@@ -1,7 +0,0 @@
-package commons;/*
- * Created by krantz on 15-02-19.
- */
-
-public class Logger {
-    //TODO
-}
---
+++ src/server/Logger.java
@@ -0,0 +1,33 @@
+package server;/*
+ * Created by krantz on 15-02-19.
+ */
+
+import java.io.*;
+
+public class Logger {
+
+    public Logger() {
+        File yourFile = new File("log.txt");
+        if (!yourFile.exists()) {
+            try {
+                yourFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public void log(String tag, String message) {
+        try {
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("log.txt", true)));
+            out.println(tag + " : " + message);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
